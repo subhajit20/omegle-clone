99d38df4eb473c266893309e8c13bef8f5cad3d4
Subhajit Ghosh <subhajitstd07@gmail.com>
2023-11-15 00:46:42 +0530

    Creating fullstack video call web app using next and express

--- frontend/src/hooks/useWebSocket.tsx
+++ frontend/src/hooks/useWebSocket.tsx
@@ -9,32 +9,31 @@ type WssTypes = {
 function useWebSocket():any {
     const [ws,setWs] = useState<WssTypes | null>(null);
 
+    const initWebSocket = (hostName:string) =>{
+        try{
+            const ws = new WebSocket(hostName);
 
-  const initWebSocket = (hostName:string) =>{
-    try{
-        const ws = new WebSocket(hostName);
-
-        setWs((prev)=>{
-            return {
-                ...prev,
-                wss:ws,
-                connected:true
-            }
-        })
-    }catch(e){
-        console.log(e)
-    }
-  }
-
-  const getOffer = (ws?:WebSocket) =>{
-    try{
-        ws!.onmessage = (e) =>{
-            console.log(JSON.parse(e.data));
+            setWs((prev)=>{
+                return {
+                    ...prev,
+                    wss:ws,
+                    connected:true
+                }
+            })
+        }catch(e){
+            console.log(e)
         }
-    }catch(e){
-        console.log(e)
     }
-  }
+
+    const getOffer = (ws?:WebSocket) =>{
+        try{
+            ws!.onmessage = (e) =>{
+                console.log(JSON.parse(e.data));
+            }
+        }catch(e){
+            console.log(e)
+        }
+    }
 
   return {
     ws,
